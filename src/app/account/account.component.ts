6cfc9246ea4ec72221b6bcdbc5a6ef531436180f
Sofia Zanetti <Zanettisofia123@gmail.com>
2021-06-29 21:42:07 -0300

    aniadiendo archivos restantes

---
+++ src/app/account/account.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.css'],
+})
+export class AccountComponent implements OnInit {
+  constructor() {
+    const profile: string =
+      'https://images.unsplash.com/photo-1573497620166-aef748c8c792?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80';
+  }
+
+  ngOnInit(): void {}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,46 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ActionsComponent } from './actions/actions.component';
+import { RouterModule } from '@angular/router';
+import { AccountComponent } from './account/account.component';
+import { ContactComponent } from './contact/contact.component';
+import { FaqComponent } from './faq/faq.component';
+import { FooterComponent } from './footer/footer.component';
+import { LandingComponent } from './landing/landing.component';
+import { ServiciosComponent } from './servicios/servicios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    ActionsComponent,
+    AccountComponent,
+    ContactComponent,
+    FaqComponent,
+    FooterComponent,
+    LandingComponent,
+    ServiciosComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    RouterModule.forRoot([
+      { path: 'actions', component: ActionsComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: 'account', component: AccountComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: 'services', component: ServiciosComponent },
+      { path: 'faq', component: FaqComponent },
+      { path: 'login', component: LoginComponent },
+      { path: '', component: LandingComponent },
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
